aae8ba626cb69e4108f73f694bf47f766ebe2443
Huy Lam <huy.lam0199@gmail.com>
2023-10-06 12:49:04 +0700

    fix config prod

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import dotenv from "dotenv";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.COOKIE_DOMAIN": JSON.stringify(process.env.COOKIE_DOMAIN),
+    "process.env.VITE_AWS_S3_BUCKET_DOMAIN": JSON.stringify(
+      process.env.VITE_AWS_S3_BUCKET_DOMAIN
+    ),
+  },
   server: {
     open: true,
     origin: "http://localhost:8080/",
