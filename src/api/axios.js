f60e4ddc58b5c702e0ee5a815d90c401d9dfde59
Huy Lam <huy.lam0199@gmail.com>
2023-10-07 12:04:44 +0700

    update env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { getToken } from "../utils/auth";
-import { apiURL } from "./propertyApi";
+// import { apiURL } from "./propertyApi";
 
 export const instance = axios.create({
-  baseURL: `${apiURL}`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: `${apiURL}`,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     "Cache-Contyrol": "no-cache",
--- src/api/propertyApi.js
+++ src/api/propertyApi.js
@@ -1,9 +1,11 @@
-export const apiURL = "https://real-estate-production-acd8.up.railway.app/";
+// export const apiURL = "https://real-estate-production-acd8.up.railway.app";
 // export const apiURL = "http://localhost:8080/";
 
 export const propertyAPI = {
   getPropertyDetail: (propertyId, type) =>
-    `${apiURL}api/v1/properties/${propertyId}${type ? `/${type}` : ""}`,
+    `${import.meta.env.VITE_API_URL}api/v1/properties/${propertyId}${
+      type ? `/${type}` : ""
+    }`,
   getProperties: (
     landType = "",
     query = "",
@@ -12,13 +14,17 @@ export const propertyAPI = {
     size = 12,
     order = "desc"
   ) =>
-    `${apiURL}api/v1/properties?sort=id,${order}&page=${page}&size=${size}&propertyPostingStatus=${query}&propertyLandType=${landType}`,
+    `${
+      import.meta.env.VITE_API_URL
+    }/api/v1/properties?sort=id,${order}&page=${page}&size=${size}&propertyPostingStatus=${query}&propertyLandType=${landType}`,
   getPropertiesSearch: (query = "") =>
-    `${apiURL}api/v1/properties?sort=id,desc&page=0&size=10&propertyPostingStatus=${query}`,
+    `${
+      import.meta.env.VITE_API_URL
+    }/api/v1/properties?sort=id,desc&page=0&size=10&propertyPostingStatus=${query}`,
   propertyImage: (path) =>
     `https://huy-property-images.s3.ap-southeast-1.amazonaws.com/${path}`,
   addPropertyPhoto: (propertyId) =>
-    `${apiURL}api/v1/photos/upload/${propertyId}`,
+    `${import.meta.env.VITE_API_URL}/api/v1/photos/upload/${propertyId}`,
   defaultImage:
     "https://cdn.dribbble.com/userupload/4220677/file/original-c7f0a15e2d639a6dc4b91665ec79ba7b.jpg?compress=1&resize=2048x1536",
 };
